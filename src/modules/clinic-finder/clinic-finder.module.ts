b4d980c67764025102fc102819e7df92fe1df8f9
Shayan Araghi <shayanaraghi@live.com>
2025-11-20 01:54:14 +0330

    refactor: remove TypeOrmModule dependency from ClinicFinderModule
    - Eliminate TypeOrmModule import as it is no longer needed
    - Simplify module structure by relying solely on CommonModule

--- src/modules/clinic-finder/clinic-finder.module.ts
+++ src/modules/clinic-finder/clinic-finder.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommonModule } from '../../common/common.module';
-import { Clinic } from '../clinics/entities/clinic.entity';
 import { ClinicFinderController } from './clinic-finder.controller';
 import { ClinicFinderService } from './clinic-finder.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Clinic]), CommonModule],
+  imports: [CommonModule],
   controllers: [ClinicFinderController],
   providers: [ClinicFinderService],
   exports: [ClinicFinderService],
